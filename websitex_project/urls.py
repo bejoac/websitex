542ba3c5667b6841bafafb3cbf3791e439787edb
Joa <betejoachim@gmail.com>
2026-04-26 18:44:07 +0200

    User can now add name and mail to database

--- websitex_project/urls.py
+++ websitex_project/urls.py
@@ -21,5 +21,5 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home),
-    path("test/", views.test),
+    path("add_user/", views.add_user),
 ]
--- websitex_project/views.py
+++ websitex_project/views.py
@@ -7,6 +7,18 @@ def home(request):
     persons = Person.objects.all()
     return render(request, "home.html", {"persons": persons})
 
-def test(request):
-    html = "<p>worked</p>"
-    return HttpResponse(html)
+def add_user(request):
+    email = request.POST.get("email")
+    first_name = request.POST.get("first_name")
+    last_name = request.POST.get("last_name")
+    print(email)
+
+    if not email: # TODO: Shouldn't be possible to Submit without Email
+        return HttpResponse("<p>Please specify E-mail</p>")
+    
+    if Person.objects.filter(email=email).exists():
+        return HttpResponse("<p>User with specified Mail already exists!</p>")
+
+    Person.objects.create(first_name=first_name, last_name=last_name, email=email) # Does this throw an error?
+
+    return HttpResponse("<p>Worked!</p>")
