4100fbeca1ad7aec99a7513a8626c3808e660f1c
Joa <betejoachim@gmail.com>
2026-04-27 11:35:37 +0200

    Imlemented htmx-oob-swap to reduce the amount of data being transferred. Before the entire persons lists would be send on every request

--- websitex_project/views.py
+++ websitex_project/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from .models import Person
 from django.middleware.csrf import get_token
 
+from django.template.loader import render_to_string
+
 def home(request):
     persons = Person.objects.all()
     return render(request, "home.html", {"persons": persons})
@@ -17,10 +19,11 @@ def add_user(request):
         return render(request, "form.html", context)
 
     Person.objects.create(first_name=first_name, last_name=last_name, email=email)
-    response = render(request, "form.html")
-    response["HX-Trigger"] = "personAdded"
 
-    return response
+    form_html = render_to_string("form.html", {}, request=request)
+    contact_html = render_to_string("oob_contact.html", {"first_name": first_name, "last_name": last_name}, request=request)
+
+    return HttpResponse(form_html + contact_html)
 
 def get_users(request):
     persons = Person.objects.all()
